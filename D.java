bc854703d355b7f123c799284afdb3c8e2a3d2c0
victorcabral29 <victor.rodrigues2@estudante.ufla.br>
2020-06-09 18:35:07 -0300

    Criação da Classe D

---
+++ D.java
@@ -0,0 +1,34 @@
+public class D {
+	
+	String D1;
+	int D2;
+	
+	public static void main (String[] args) {
+		
+	}
+	
+	public void setD1(String umD1){
+		this.D1 = umD1;
+	}
+	
+	public void setD2(int umD2){
+		this.D2 = umD2;
+	}
+	
+	public String getD1(){
+		return D1;
+	}
+	
+	public int getD2(){
+		return D2;
+	}
+	
+	public String MD1(){
+		return ("MD1");
+	}
+	
+	public String MD2(){
+		return ("MD2");
+	}
+	
+}
